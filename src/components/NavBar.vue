a348fb2d9b883aaca41ecf46e4b888fff9060f53
Julio Garcia <jgarc5394@gmail.com>
2023-10-01 14:48:31 -0500

    Added .env file

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,7 +4,7 @@
 
     const { isAuthenticated, logout, user } = useAuth()
 
-    const brand = ref('🖥️ Fictional Company Directory')
+    const brand = ref(import.meta.env.VITE_APP_NAME)
 </script>
 
 <template>
